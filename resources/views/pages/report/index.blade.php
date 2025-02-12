8be086cf9279c9073dd20ba231c6f471095e19c2
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2025-02-12 18:21:10 +0700

    prepare laporan

--- resources/views/pages/report/index.blade.php
+++ resources/views/pages/report/index.blade.php
@@ -6,14 +6,56 @@
 
 @section('content')
   <section class="content">
-    <div class="container-fluid">
-      <div class="row">
-        <div clas="col-12">
-          <ul>
-            <li><a href="#">Laporan 1</a></li>
-            <li><a href="#">Laporan 2</a></li>
-            <li><a href="#">Laporan 3</a></li>
-          </ul>
+    <div class="card">
+      <div class="card-body">
+        <div class="container-fluid">
+          <div class="row">
+            <div clas="col-12">
+              <h3>Laporan Keuangan Utama</h3>
+              <ul>
+                <li><a href="{{ url('report/') }}">Laporan Rekapitulasi Pemasukan dan Pengeluaran</a></li>
+
+                {{--
+                    Laporan Arus Kas (Cash Flow) - Penting untuk Memantau Keuangan
+                    Bisa disaring berdasarkan periode, kategori, akun
+                    - Total pemasukan & pengeluaran dalam periode tertentu.
+                    - Saldo awal dan saldo akhir periode tersebut.
+                    - Bisa dikelompokkan berdasarkan kategori (opsional).
+                --}}
+                <li><a href="{{ url('report/') }}">Laporan Arus Kas</a></li>
+
+                {{--
+                    Laporan Pemasukan & Pengeluaran - Untuk Mengetahui Sumber Keuangan
+                    Bisa disaring berdasarkan periode dan akun
+                    Isi laporan:
+                    - Total pemasukan dari berbagai sumber.
+                    - Total pengeluaran berdasarkan kategori utama.
+                --}}
+                <li><a href="{{ url('report/') }}">Laporan Pemasukan & Pengeluaran</a></li>
+
+                {{--
+                    Laporan Saldo Akun - Untuk Melihat Uang di Setiap Akun
+                    Isi laporan:
+                    - Saldo akhir di setiap akun kas/bank.
+                --}}
+                <li><a href="{{ url('report/') }}">Laporan Saldo Akun</a></li>
+              </ul>
+
+              <h3>Laporan Rincian Transaksi</h3>
+              <ul>
+                <li><a href="{{ url('report/') }}">Laporan Transaksi berdasarkan Periode</a></li>
+                <li><a href="{{ url('report/') }}">Laporan Pemasukan</a></li>
+                <li><a href="{{ url('report/') }}">Laporan Pengeluaran</a></li>
+              </ul>
+
+              <h3>Laporan Analitik dan Insight</h3>
+              <ul>
+                <li><a href="{{ url('report/') }}">Laporan Tren Keuangan</a></li>
+                <li><a href="{{ url('report/') }}">Laporan Kategori Terbanyak</a></li>
+                <li><a href="{{ url('report/') }}">Laporan Perbandingan Bulanan / Tahunan</a></li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
