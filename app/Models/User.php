23d6b1d7ceb5f3c18ab2db8010722ced1361492f
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2025-02-15 07:26:25 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,11 +18,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable
 {
+    use HasFactory;
+
     public $timestamps = false;
 
     /**
